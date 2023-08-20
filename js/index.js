8ea3d8bb20be6c4619f42ad78b9b0e3402f4a392
Sabrina Bertol <bertol.sabrina@gmail.com>
2023-08-21 00:21:57 +0100

    added header, made responsive, cleaned the code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// drawing canvas
+// Drawing canvas
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -27,41 +27,25 @@ canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
 
-
-
-
-
-// slideshow
+// Slideshow
 let slideshows = document.querySelectorAll('[data-component="slideshow"]');
-  
-// Apply to all slideshows that you define with the markup wrote
 slideshows.forEach(initSlideShow);
-
 function initSlideShow(slideshow) {
-
-  let slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
-
+  let slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); 
   let index = 0, time = 5000;
   slides[index].classList.add('active');  
-  
   setInterval( () => {
     slides[index].classList.remove('active');
-    
-    //Go over each slide incrementing the index
     index++;
-    
-    // If you go over all slides, restart the index to show the first slide and start again
     if (index === slides.length) index = 0; 
-    
     slides[index].classList.add('active');
-
   }, time);
 }
 
+
+//Shiny text
 const quote = document.querySelector(".shiny");
-
 const readout = document.querySelector("p");
-
 quote.addEventListener("mousemove", (e) => {
   const { x, y } = quote.getBoundingClientRect();
   quote.style.setProperty("--x", e.clientX - x);
